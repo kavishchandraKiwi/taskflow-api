c0779494e67ecd2a67b1d4e5bbb442e192ebdc32
Kavish Chandra <kavish.chandra@kiwiinsurance.com>
2026-07-13 04:00:53 +0530

    tasks DTO,scaffolding

---
+++ src/tasks/dto/update-task.dto.ts
@@ -0,0 +1,8 @@
+export class UpdateTaskDto {
+  title?: string;
+  description?: string;
+  priority?: 'low' | 'medium' | 'high';
+  status?: 'todo' | 'in_progress' | 'done';
+  due_date?: string | Date;
+  assigned_to_user_id?: number | null;
+}
